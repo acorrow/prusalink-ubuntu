62352da9b4a11ae20d6c5f1e74f4d6ad3056fea0
Adam Corrow <adam@dualcorsoftware.com>
2023-02-15 13:41:21 -0500

    Update

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # prusalink-ubuntu
-Automated setup of a PrusaLink system on Ubuntu Server
+## Automated PrusaLink Ubuntu Setup
 
 NOTE: You will need to generate a GitHub Access Token. This will allow the script here to make a call to add your SSH Key from the Ubuntu server to GitHub, so you can SSH Clone Repos.
+
+### setup.sh
 
 Simply run the following command to use this script (Replace `MY_GITHUB_ACCESS_TOKEN` with your actual token generated above):
 
 `sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/acorrow/prusalink-ubuntu/main/setup.sh)" -- "$MY_GITHUB_ACCESS_TOKEN"`
 
+### img2neo.sh
+
+This script will take a PNG/JPG and rasterize it into ASCII art that is colorable via Neofetch. Specifically, normal ANSI art might have a series of escaped color sequences to display. Whereas Neofetch uses a custom format. This script uses image conversion to ANSI colors, and extracts them into the format Neofetch is using.
+
+Usage:
+`img2neo.sh [file.png] [numberOfLines] [colorDepth]`
+Example:
+`img2neo.sh somePng.png 20 4`
+
+The numberOfLines simply limits the size of the file. 20 is generally good, but depending on your overall Neofetch setup, you might want to use something different.
+
+This creates a file called `asciiLogo.txt` in the directory it was run in.
--- setup.sh
+++ setup.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 #Setup USB Access to printer:
+
+#Flags for testing
+devMode=true;
+if [ "$devMode" = true ]; then
+  echo "Test Mode"
+else
+  echo "Setting up Prusa Link For Ubuntu"
+fi
 GHT=$1
+if [ -z "$GHT" ]; then
+  echo "Warning! No GitHub Token supplied!"
+    if [ "$devMode" = true ]; then
+        echo "But... You are in TestMode so whatever..."
+    else
+        echo "Exiting..."
+        exit;
+    fi
+else
+  echo "This script will create (or use) your SSH key with GitHub"
+fi
+
+echo "Setting up etc/Prusa-Link/prusa-link.ini - This allows for USB access to the Printer"
+
 DIRECTORY="/etc/Prusa-Link"
 FILE="prusa-link.ini"
 CONTENT="[printer]\nport=/dev/ttyACM0\nbaudrate=115200"
@@ -11,15 +33,23 @@ if [ -f "$DIRECTORY/$FILE" ]; then
   rm "$DIRECTORY/$FILE"
 fi
 echo -e "$CONTENT" > "$DIRECTORY/$FILE"
+
 #generate ssh key and export public key to term
 if [ ! -f ~/.ssh/id_rsa ]; 
+    echo "No SSH Key exists on this machine. Generating..."
     then ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa; 
 fi
+echo "Exporting SSH Key to GitHub"
 sshKey=$(cat ~/.ssh/id_rsa.pub)
 #Setup a GitHub SSH key so you can easily clone the repos...
-GHT="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"
-curl -X POST -H "Authorization: token $GHT" -d "{"title":"prusaLinkSSHKey","key":"$sshKey"}" https://api.github.com/user/keys
+if [ -z "$GHT" ]; then
+    echo "Skipping the SSH Key add to Git Hub. Hope you already have one...."
+else
+    curl -X POST -H "Authorization: token $GHT" -d "{"title":"prusaLinkSSHKey","key":"$sshKey"}" https://api.github.com/user/keys
+fi
+
 ##TODO Remove Welcome Message
+
 git clone git@github.com:prusa3d/Prusa-Link.git
 git clone https://github.com/prusa3d/Prusa-Connect-SDK-Printer
 
@@ -27,10 +57,13 @@ git clone https://github.com/prusa3d/Prusa-Connect-SDK-Printer
 sudo apt-get install jp2a libturbojpeg0-dev libcap-dev
 sudo apt install python3-pip
 sudo apt install neofetch
-#git clone both repos
 
-
-
-#theme items
-#prompt color
+#Prompt Color
 #bashrc PS1 variable = prompt colors
+#[38;5;208m\]
+#That is the color we need to grep/replace in the PS1 for Ubuntu bash...
+
+
+#Actually install Prusa-Link
+sudo pip3 install Prusa-Connect-SDK-Printer/.
+sudo pip3 install Prusa-Link/.

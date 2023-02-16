dd24260b3d520dd8e4f08c0a273948c0f4c7add5
Adam Corrow <adam@dualcorsoftware.com>
2023-02-16 01:29:10 -0500

    asdf

--- srvTest.sh
+++ srvTest.sh
@@ -1,33 +1,64 @@
+systemctl stop prusa-link.service
+systemctl stop wlan0-redirect.service
+systemctl stop eth0-redirect.service
+
+sudo rm /etc/systemd/system/prusa-link.service
+sudo rm /etc/systemd/system/wlan0-redirect.service
+sudo rm /etc/systemd/system/eth0-redirect.service
+
 ServiceDir="/etc/systemd/system"
-ServiceFile="prusa-link.service"
 
-if [ ! -d "$ServiceDir" ]; then
-  mkdir -p "$ServiceDir"
-fi
-if [ -f "$ServiceDir/$ServiceFile" ]; then
-  rm "$ServiceDir/$ServiceFile"
-fi
-echo -e "$CONTENT" >"$ServiceDir/$ServiceFile"
+sudo tee "/etc/systemd/system/prusa-link.service" > /dev/null <<EOF
+[Unit]
+Description=Prusa Link Service
+After=network.target
 
-file="$ServiceDir/$ServiceFile"
-{ 
-    echo "[Unit]";
-    echo "Description=Prusa Link Service";
-    echo "";
-    echo "[Service]";
-    echo "ExecStart='iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080'; /usr/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080; printf 'M117 Starting Prusa Link' > /dev/ttyAMA0; rm -f /home/pi/prusa-link.pid; export PYTHONOPTIMIZE=2; su pi -c '/home/pi/.local/bin/prusa-link -i start'";
-    echo "Type=oneshot";
-    echo "RemainAfterExit=yes";
-    echo "[Install]";
-    echo "WantedBy=multi-user.target";
-  
-} | sudo tee $file
+[Service]
+ExecStart=/home/pi/.local/bin/prusa-link -i start
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo tee "/etc/systemd/system/wlan0-redirect.service" > /dev/null <<EOF
+[Unit]
+Description=IPTables Redirect Service
+After=network.target
+
+[Service]
+ExecStart=/usr/sbin/iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo tee "/etc/systemd/system/eth0-redirect.service" > /dev/null <<EOF
+[Unit]
+Description=IPTables Redirect Service
+After=network.target
+
+[Service]
+ExecStart=/usr/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 
 
 
 # Reload the systemd daemon and start the service
 systemctl daemon-reload
+systemctl start wlan0-redirect.service
+systemctl start eth0-redirect.service
 systemctl start prusa-link.service
 
 # Enable the service to start at boot
 systemctl enable prusa-link.service
+systemctl enable eth0-redirect.service
+systemctl enable wlan0-redirect.service

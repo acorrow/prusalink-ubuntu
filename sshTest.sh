aa7ad9bb8184a6cb4c677b132c1afecd2bb29e6a
Adam Corrow <adam@dualcorsoftware.com>
2023-02-15 21:26:03 -0500

    roooooot

--- sshTest.sh
+++ sshTest.sh
@@ -2,7 +2,7 @@
 GHT=$1
 #Setup USB Access to printer:
 #generate ssh key and export public key to term
-if [ ! -e ~/.ssh/id_rsa ]; 
+if [ ! -e /root/.ssh/id_rsa ]; 
     echo "No SSH Key exists on this machine. Generating..."
     then ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa; 
 fi

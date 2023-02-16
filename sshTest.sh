57a734ff4d8ea9490b644e2bd9b3b6d14d6dfcf8
Adam Corrow <adam@dualcorsoftware.com>
2023-02-15 21:34:34 -0500

    re rooot

--- sshTest.sh
+++ sshTest.sh
@@ -2,11 +2,9 @@
 GHT=$1
 #Setup USB Access to printer:
 #generate ssh key and export public key to term
-path="/root/.ssh/id_rsa"
-if [ ! -e $path ]; 
-    echo $path
+if [ ! -f /root/.ssh/id_rsa ]; 
     echo "No SSH Key exists on this machine. Generating..."
-    then ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa; 
+    then ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa; 
 fi
 echo "Exporting SSH Key to GitHub"
 sshKey=$(cat ~/.ssh/id_rsa.pub)
@@ -14,8 +12,7 @@ sshKey=$(cat ~/.ssh/id_rsa.pub)
 if [ -z "$GHT" ]; then
     echo "Skipping the SSH Key add to Git Hub. Hope you already have one...."
 else
-    echo $sshKey
-    http --print BbHh --form POST https://api.github.com/user/keys \
+    http --form POST https://api.github.com/user/keys \
         Authorization:"token $GHT" \
         title="prusaLinkSSHKey" \
         key="$sshKey"

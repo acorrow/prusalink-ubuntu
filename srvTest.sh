0ec2e9ab0659802f74db5bcff76fd3c105ebe635
Adam Corrow <adam@dualcorsoftware.com>
2023-02-16 00:06:01 -0500

    CHutny

--- srvTest.sh
+++ srvTest.sh
@@ -1,23 +1,29 @@
-DIRECTORY="/etc/systemd/system"
-FILE="prusa-link.service"
-CONTENT="[Unit]
-Description=Prusa Link Service
-After=network.target
+ServiceDir="/etc/systemd/system"
+ServiceFile="prusa-link.service"
 
-[Service]
-ExecStart=/bin/bash -c \"/usr/sbin/iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080; /usr/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080; printf 'M117 Starting Prusa Link\n' > /dev/ttyAMA0; rm -f /home/pi/prusa-link.pid; export PYTHONOPTIMIZE=2; su pi -c '/home/pi/.local/bin/prusa-link -i start'\"
-Type=oneshot
-RemainAfterExit=yes
+if [ ! -d "$ServiceDir" ]; then
+  mkdir -p "$ServiceDir"
+fi
+if [ -f "$ServiceDir/$ServiceFile" ]; then
+  rm "$ServiceDir/$ServiceFile"
+fi
+echo -e "$CONTENT" >"$ServiceDir/$ServiceFile"
 
-[Install]
-WantedBy=multi-user.target"
-if [ ! -d "$DIRECTORY" ]; then
-  mkdir -p "$DIRECTORY"
-fi
-if [ -f "$DIRECTORY/$FILE" ]; then
-  rm "$DIRECTORY/$FILE"
-fi
-echo -e "$CONTENT" >"$DIRECTORY/$FILE"
+file="$ServiceDir/$ServiceFile"
+{ 
+    echo "[Unit]";
+    echo "Description=Prusa Link Service";
+    echo "";
+    echo "[Service]";
+    echo "ExecStart=/bin/bash -c \"/usr/sbin/iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080; /usr/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080; printf 'M117 Starting Prusa Link\n' > /dev/ttyAMA0; rm -f /home/pi/prusa-link.pid; export PYTHONOPTIMIZE=2; su pi -c '/home/pi/.local/bin/prusa-link -i start'\"";
+    echo "Type=oneshot";
+    echo "RemainAfterExit=yes";
+    echo "[Install]";
+    echo "WantedBy=multi-user.target";
+  
+} | sudo tee $file
+
+
 
 # Reload the systemd daemon and start the service
 systemctl daemon-reload

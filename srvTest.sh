a275439c70f67f45a8e7e5248aa9a9245702df57
Adam Corrow <adam@dualcorsoftware.com>
2023-02-16 01:00:17 -0500

    stuff

--- srvTest.sh
+++ srvTest.sh
@@ -15,7 +15,7 @@ file="$ServiceDir/$ServiceFile"
     echo "Description=Prusa Link Service";
     echo "";
     echo "[Service]";
-    echo "ExecStart=iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080; /usr/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080; printf 'M117 Starting Prusa Link\n' > /dev/ttyAMA0; rm -f /home/pi/prusa-link.pid; export PYTHONOPTIMIZE=2; su pi -c '/home/pi/.local/bin/prusa-link -i start'";
+    echo "ExecStart=iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080; /usr/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080; printf 'M117 Starting Prusa Link\\n' > /dev/ttyAMA0; rm -f /home/pi/prusa-link.pid; export PYTHONOPTIMIZE=2; su pi -c '/home/pi/.local/bin/prusa-link -i start'";
     echo "Type=oneshot";
     echo "RemainAfterExit=yes";
     echo "[Install]";

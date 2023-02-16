e8d950862a5d33f0d6cc762f23457b26d37fd75f
Adam Corrow <adam@dualcorsoftware.com>
2023-02-15 21:47:41 -0500

    curl

--- sshTest.sh
+++ sshTest.sh
@@ -20,9 +20,9 @@ sshKey=$(cat ~/.ssh/id_rsa.pub)
 if [ -z "$GHT" ]; then
     echo "Skipping the SSH Key add to Git Hub. Hope you already have one...."
 else
-    http --print HhBb --form POST https://api.github.com/user/keys \
-        Authorization:"token $GHT" \
-        title="prusaLinkSSHKey" \
-        key="$sshKey"
-    #curl -X POST -H "Authorization: token $GHT" -d "{"title":"prusaLinkSSHKey","key":"$sshKey"}" https://api.github.com/user/keys
+    # http --print HhBb --form POST https://api.github.com/user/keys \
+    #     Authorization:"token $GHT" \
+    #     title="prusaLinkSSHKey" \
+    #     key="$sshKey"
+    curl -X POST -H "Authorization: token $GHT" -d "{"title":"prusaLinkSSHKey","key":"$sshKey"}" https://api.github.com/user/keys
 fi
